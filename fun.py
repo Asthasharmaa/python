2566aa27b4c47bbec64901ed36cdf8bc4970c3b4
Asthasharmaa <39943691+Asthasharmaa@users.noreply.github.com>
2018-06-08 10:24:00 +0000

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+import time
+#defining function
+def motd():
+   print"hello all,time is",time.ctime()
+#function with argument
+def add(x,y):
+   print"x+y"
+def new():
+   print"/n"*50
+motd()
+add(3,7)
+new()
